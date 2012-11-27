622a1057091ef25f45984b0c3e78aad3438109bb
James Ryan Cresawn <jrcresawn@gmail.com>
2012-11-27 08:01:18 -0700

    First working version of an HTTP client.

--- client.py
+++ client.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
 import httplib
+import urllib
 import json
 import sys
 
 def update():
+    params = urllib.urlencode({'ready': True})
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
     conn = httplib.HTTPConnection("acic2012.iplantcollaborative.org")
-    conn.endheaders(message_body=None)
-    conn.send(json.dumps({"ready": True}))
+    conn.request("POST", "/cgi-bin/server.py", params, headers)
     r = conn.getresponse()
     if r.status == 200:
         data = r.read()
+        print data
         conn.close()
     else:
         sys.exit(1)
 
-update
+update()
